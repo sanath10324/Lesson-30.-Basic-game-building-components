108a0402fd8bbbac2fa0a2fd835f4fd3f3b0f65c
sanath10324 <sanathkatkamgamer@gmail.com>
2026-07-02 10:42:21 +0400

    Adding elements to the screen

--- add_elements_to_the_screen.py
+++ add_elements_to_the_screen.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
-pygame.display.set_caption("My first game screen")
+pygame.display.set_caption("game screen")
 
 
 WHITE = (255, 255, 255)
